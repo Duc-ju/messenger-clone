acb49a2616f5ab54d120e930e94e569176555eae
Duc-ju <trangduc56@gmail.com>
2022-02-28 13:42:11 +0700

    update style of timestamp in room list view

--- src/logic/getRangeOfTimeToCurrent.js
+++ src/logic/getRangeOfTimeToCurrent.js
@@ -2,10 +2,13 @@ export function getRangeOfTimeToCurrent(second) {
   const current = new Date();
   let rangeMiliSecond = current - second * 1000;
   let seconds = Math.round(rangeMiliSecond / 1000);
-  let minutes = Math.round(rangeMiliSecond / 1000 / 60);
-  let hours = Math.round(rangeMiliSecond / 1000 / 60 / 60);
   if (seconds < 30) return 'vừa xong';
   if (seconds < 60) return seconds + ' giây';
+  let minutes = Math.round(rangeMiliSecond / 1000 / 60);
   if (minutes < 60) return minutes + ' phút';
-  return hours + ' giờ';
+  let hours = Math.round(rangeMiliSecond / 1000 / 60 / 60);
+  if (hours < 24) return hours + ' giờ';
+  let days = Math.round(rangeMiliSecond / 1000 / 60 / 60 / 24);
+  if (days < 7) return days + ' ngày';
+  return Math.round(rangeMiliSecond / 1000 / 60 / 60 / 24 / 7) + ' tuần';
 }
